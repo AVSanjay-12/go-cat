4176b4dcd0222afa74386b2bbe51961b352d3e6f
Sanjay A V <sanjayav1262@gmail.com>
2025-05-07 07:22:49 +0530

    open files from cmd

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
+	"io"
 	"os"
 )
 
+// Initialize flag variables
 var (
 	flagN, flagB, flagE, flagT, flagV, flagA, flagE2, flagT2, help bool
 )
 
+// Assign flag to the corresponding variables
 func init() {
 	flag.BoolVar(&flagN, "n", false, "Number all lines")
 	flag.BoolVar(&flagB, "b", false, "Number non empty lines")
@@ -22,9 +27,57 @@ func init() {
 }
 
 func main() {
+	// Pass command line arguments
 	flag.Parse()
 	if help || !flag.Parsed(){
 		flag.Usage()
 		os.Exit(0)
 	}
+
+	if flagA{
+		flagV = true
+		flagE = true
+		flagT = true
+	}
+	if flagE2{
+		flagV = true
+		flagE = true
+	}
+	if flagT2{
+		flagV = true
+		flagT = true
+	}
+
+	// Getting files from cmd line
+	files := flag.Args()
+	if len(files) == 0{
+		files = []string{"-"}
+	}
+
+	lineNum := 0
+	// Go through the file names
+	for _, fname := range files{
+		var r io.Reader
+		if fname == "-"{
+			r := os.Stdin
+		} else{
+			file, err := os.Open(fname)
+			if err != nil{
+				fmt.Fprintf(os.Stderr, "%v\n", err)
+				continue
+			}
+			defer file.Close()
+			r = file
+		}
+
+		// Now print the content on each file
+		printFile(r, &lineNum)
+	}
+}
+
+// Go through the file
+func printFile(r io.Reader, lineNum *int){
+	scanner := bufio.NewScanner(r)
+
+
 }
